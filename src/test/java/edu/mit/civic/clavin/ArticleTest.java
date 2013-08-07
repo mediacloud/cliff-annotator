d7cba9d803d7448f6b149266184e419c52e7ad64
Rahul Bhargava <rahulb@media.mit.edu>
2013-08-07 15:00:23 -0400

    more real test cases

--- src/test/java/edu/mit/civic/clavin/ArticleTest.java
+++ src/test/java/edu/mit/civic/clavin/ArticleTest.java
@@ -1,10 +1,13 @@
 package edu.mit.civic.clavin;
 
+import static org.junit.Assert.assertTrue;
+
 import java.io.File;
-import java.io.IOException;
+import java.util.List;
 
 import org.junit.Test;
 
+import com.berico.clavin.resolver.ResolvedLocation;
 import com.berico.clavin.util.TextUtils;
 
 import edu.mit.civic.clavin.server.ParseManager;
@@ -12,16 +15,41 @@ import edu.mit.civic.clavin.server.ParseManager;
 /**
  */
 public class ArticleTest {
-
-    /**
-     * @throws IOException 
-     */
+ 
     @Test
-    public void testSpeech() throws Exception {
+    public void testMinnesotaExample() throws Exception {
+        File inputFile = new File("src/test/resources/sample-docs/minnesota.txt");
+        String inputString = TextUtils.fileToString(inputFile);
+        List<ResolvedLocation> results = ParseManager.locateRaw(inputString);
+        assertTrue(resultsContainsPlaceId(results, 5037779));   // has Minnesota
+    }
+
+    @Test
+    public void testUsExample() throws Exception {
         File inputFile = new File("src/test/resources/sample-docs/speech.txt");
         String inputString = TextUtils.fileToString(inputFile);
-        String results = ParseManager.locate(inputString);
-        System.out.println(results);
+        List<ResolvedLocation> results = ParseManager.locateRaw(inputString);
+        assertTrue(resultsContainsPlaceId(results, 5037779));   // has Minnesota
+        assertTrue(resultsContainsPlaceId(results, 1816670));   // has city of Beijing
+    }
+
+    @Test
+    public void testCountryExample() throws Exception {
+        File inputFile = new File("src/test/resources/sample-docs/multi-country.txt");
+        String inputString = TextUtils.fileToString(inputFile);
+        List<ResolvedLocation> results = ParseManager.locateRaw(inputString);
+        assertTrue(resultsContainsPlaceId(results, 1814991));   // country of China
+        assertTrue(resultsContainsPlaceId(results, 1269750));   // country of India
+        assertTrue(resultsContainsPlaceId(results, 2077456));   // country of Australia
+    }
+
+    private boolean resultsContainsPlaceId(List<ResolvedLocation> results, int placeId){
+        for(ResolvedLocation location: results){
+            if(location.getPlace().getId()==placeId){
+                return true;
+            }
+        }
+        return false;
     }
     
 }
