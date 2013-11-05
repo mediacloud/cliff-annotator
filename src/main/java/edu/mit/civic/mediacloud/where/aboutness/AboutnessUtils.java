3d4c7dde812ce6ea238ed31b3f9d9a41218d82b3
kanarinka <kanarinka@gmail.com>
2013-11-05 15:51:31 -0500

    switching back to 10%

--- src/main/java/edu/mit/civic/mediacloud/where/aboutness/AboutnessUtils.java
+++ src/main/java/edu/mit/civic/mediacloud/where/aboutness/AboutnessUtils.java
@@ -26,20 +26,24 @@ public class AboutnessUtils {
         HashMap<CountryCode,Integer> countryCounts = new HashMap<CountryCode,Integer>();
         
         //This is a rough sentence parsing hack to deal with test data - doesn't take into account !? - re-do once we have MediaCloud sentences again
+        //For testing different approaches to this strategy
         int headlineAndFirstSentenceIdx = text.indexOf('.');
         int secondSentenceIdx = text.indexOf('.', headlineAndFirstSentenceIdx);
         int thirdSentenceIdx = text.indexOf('.', secondSentenceIdx);
+        int fourthSentenceIdx = text.indexOf('.', thirdSentenceIdx);
+        int fifthSentenceIdx = text.indexOf('.', fourthSentenceIdx);
         
         for (ResolvedLocation resolvedLocation: resolvedLocations){
             if(resolvedLocation.geoname.primaryCountryCode==CountryCode.NULL){
                 continue;
             }
             int position = resolvedLocation.location.position;
-            int percent10 = ((Double)(text.length() * 0.1)).intValue();
+            int percent10 = text.length()/10;
+            
             int points = 1;
             if( position <= percent10){
             	points = 2;	
-            }
+            } 
             
             CountryCode country = resolvedLocation.geoname.primaryCountryCode;
             if(!countryCounts.containsKey(country)){
