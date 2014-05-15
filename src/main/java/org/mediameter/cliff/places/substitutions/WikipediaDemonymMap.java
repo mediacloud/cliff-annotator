08f76c5d85af454a70f33b77fbb8656b392b5814
Rahul Bhargava <rahulb@media.mit.edu>
2014-05-15 13:17:04 -0400

    cleanup imports

--- src/main/java/org/mediameter/cliff/places/substitutions/WikipediaDemonymMap.java
+++ src/main/java/org/mediameter/cliff/places/substitutions/WikipediaDemonymMap.java
@@ -7,9 +7,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Map.Entry;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
