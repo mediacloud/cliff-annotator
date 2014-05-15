adf9685bb2a91b91f80bdf31c239e7b98b15d7ee
Rahul Bhargava <rahulb@media.mit.edu>
2014-05-15 15:36:51 -0400

    v1.0.0rc1

--- src/main/java/org/mediameter/cliff/ParseManager.java
+++ src/main/java/org/mediameter/cliff/ParseManager.java
@@ -33,7 +33,7 @@ public class ParseManager {
      * Minor: change in json result format
      * Revision: minor change or bug fix
      */
-    static final String PARSER_VERSION = "1.0.0rc";
+    static final String PARSER_VERSION = "1.0.0rc1";
     
     private static final Logger logger = LoggerFactory.getLogger(ParseManager.class);
 
