1775d29530ac7d7568eb5498761c0f505587d294
Rahul Bhargava <rahulb@media.mit.edu>
2014-08-19 10:04:05 -0400

    more nlp prep

--- src/test/java/org/mediameter/cliff/test/MuckUtilsTest.java
+++ src/test/java/org/mediameter/cliff/test/MuckUtilsTest.java
@@ -16,36 +16,17 @@ public class MuckUtilsTest {
     @Before
     public void setUp() throws Exception {
     }
-
-    @Test
-    public void testOnlyLocations() throws IOException {
-        ExtractedEntities entities = loadTestFile("nlp-test-locations.json");
-        assertEquals("Wrong number of location occurrences", 3, entities.getLocations().size());
-    }
-    
-    @Test
-    public void testNoEntities() throws IOException {
-        ExtractedEntities entities = loadTestFile("nlp-test-nothing.json");
-        assertEquals("Wrong number of location occurrences", 0, entities.getLocations().size());
-        assertEquals("Wrong number of people occurrences", 0, entities.getPeople().size());
-    }
-    
-    @Test
-    public void testOnlyPeople() throws IOException {
-        ExtractedEntities entities = loadTestFile("nlp-test-people.json");
-        assertEquals("Wrong number of people occurrences", 1, entities.getPeople().size());
-    }
     
     @Test
     public void testEverything() throws IOException {
-        ExtractedEntities entities = loadTestFile("nlp-test-everything.json");
-        assertEquals("Wrong number of location occurrences", 1, entities.getLocations().size());
-        assertEquals("Wrong number of people occurrences", 1, entities.getPeople().size());
+        String fileName = "nlp-test-1.json";
+        File file = new File("src/test/resources/sample-muck-json/"+fileName); 
+        String json = FileUtils.readFileToString(file);
+        
+        ExtractedEntities entities  = MuckUtils.entitiesFromJsonString(json);
+        assertEquals("Wrong number of location occurrences", 5, entities.getLocations().size());
+        assertEquals("Wrong number of people occurrences", 13, entities.getPeople().size());
+        assertEquals("Wrong number of organization occurrences", 3, entities.getOrganizations().size());
     }
     
-    private ExtractedEntities loadTestFile(String fileName) throws IOException{
-        String json = FileUtils.readFileToString(new File("src/test/resources/sample-muck-json/"+fileName));
-        return MuckUtils.entitiesFromJsonString(json);
-    }
-
 }
