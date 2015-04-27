e98332690b64ce4cf4be5965a8d45db75aaaac28
Rahul Bhargava <rahulb@media.mit.edu>
2015-04-27 10:26:36 -0400

    more test cases and debug logging

--- src/test/java/org/mediameter/cliff/test/places/disambiguation/ArlingtonVirginiaPlaceTest.java
+++ src/test/java/org/mediameter/cliff/test/places/disambiguation/ArlingtonVirginiaPlaceTest.java
@@ -10,17 +10,23 @@ import org.mediameter.cliff.test.util.TestPlaces;
 
 import com.bericotech.clavin.resolver.ResolvedLocation;
 
+/**
+ * Problem case reported by Guy Demeter
+ */
 public class ArlingtonVirginiaPlaceTest {
 
-    @Test
-    /**
-     * Problem case reported by Guy Demeter
-     */
     public void test10kReportMention() throws Exception{
         List<ResolvedLocation> results = ParseManager.extractAndResolve("I am from Arlington, (VA|Virginia).").getResolvedLocations();
         assertEquals("Found "+results.size()+" places, should have been 2!",2,results.size());
         assertEquals(TestPlaces.STATE_VIRGINIA,results.get(0).geoname.geonameID);
         assertEquals(TestPlaces.CITY_ARLINGTON_VIRGINIA,results.get(1).geoname.geonameID);
     }    
+    
+    @Test
+    public void testAbbreviation() throws Exception{
+        List<ResolvedLocation> results = ParseManager.extractAndResolve("I am from Arlington, VA.").getResolvedLocations();
+        assertEquals("Found "+results.size()+" places, should have been 1!",1,results.size());
+        assertEquals(TestPlaces.STATE_VIRGINIA,results.get(0).geoname.geonameID);
+    }
     
 }
---
+++ src/test/java/org/mediameter/cliff/test/places/disambiguation/FranceNLPPlaceTest.java
@@ -0,0 +1,36 @@
+package org.mediameter.cliff.test.places.disambiguation;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import org.mediameter.cliff.ParseManager;
+import org.mediameter.cliff.extractor.ExtractedEntities;
+import org.mediameter.cliff.test.util.TestPlaces;
+import org.mediameter.cliff.util.MuckUtils;
+
+import com.bericotech.clavin.resolver.ResolvedLocation;
+
+public class FranceNLPPlaceTest {
+    
+    @SuppressWarnings("rawtypes")
+    @Test
+    public void testFranceFromNlp() throws Exception{
+        String fileName = "story307385477.json";
+        File file = new File("src/test/resources/sample-muck-json/"+fileName); 
+        String json = FileUtils.readFileToString(file);
+        ExtractedEntities entities  = MuckUtils.entitiesFromNlpJsonString(json);
+        assertEquals("Wrong number of location occurrences", 5, entities.getLocations().size());
+        assertEquals("Wrong number of people occurrences", 18, entities.getPeople().size());
+        assertEquals("Wrong number of organization occurrences", 8, entities.getOrganizations().size());
+        entities = ParseManager.getParserInstance().resolve(entities);
+        List<ResolvedLocation> results = entities.getResolvedLocations();
+        assertEquals("Found "+results.size()+" places, should have been 5!",5,results.size());
+        assertEquals(TestPlaces.COUNTRY_IRELAND,results.get(0).geoname.geonameID);
+        assertEquals(TestPlaces.COUNTRY_FRANCE,results.get(1).geoname.geonameID);
+    }
+
+}
