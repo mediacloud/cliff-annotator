c68511fd47a30d8238b3fe5c55c3bcf6534c1a75
Rahul B <rahulbot@gmail.com>
2017-10-03 13:29:30 -0400

    unit test tweak

--- stanford-entity-extractor/src/test/java/org/mediameter/cliff/test/places/GeoNameAncestryTest.java
+++ stanford-entity-extractor/src/test/java/org/mediameter/cliff/test/places/GeoNameAncestryTest.java
@@ -17,21 +17,17 @@ public class GeoNameAncestryTest {
     public static int GEONAME_USA = 6252001;
     
     @Test
-    public void test() {
-        try {
-            GeoName mit = ParseManager.getGeoName(GEONAME_MIT);
-            assertTrue(mit.isAncestryResolved());
-            GeoName mitParent1 = mit.getParent();
-            assertEquals(GEONAME_MIDDLESEX_COUNTY,mitParent1.getGeonameID());
-            GeoName mitParent2 = mitParent1.getParent();
-            assertEquals(GEONAME_MASSACHUSETTS_STATE,mitParent2.getGeonameID());
-            GeoName mitParent3 = mitParent2.getParent();
-            assertEquals(GEONAME_USA,mitParent3.getGeonameID());
-            GeoName mitParent4 = mitParent3.getParent();
-            assertEquals(null,mitParent4);
-        } catch (UnknownGeoNameIdException e) {
-            e.printStackTrace();
-        }
+    public void test() throws UnknownGeoNameIdException {
+        GeoName mit = ParseManager.getGeoName(GEONAME_MIT);
+        assertTrue(mit.isAncestryResolved());
+        GeoName mitParent1 = mit.getParent();
+        assertEquals(GEONAME_MIDDLESEX_COUNTY,mitParent1.getGeonameID());
+        GeoName mitParent2 = mitParent1.getParent();
+        assertEquals(GEONAME_MASSACHUSETTS_STATE,mitParent2.getGeonameID());
+        GeoName mitParent3 = mitParent2.getParent();
+        assertEquals(GEONAME_USA,mitParent3.getGeonameID());
+        GeoName mitParent4 = mitParent3.getParent();
+        assertEquals(null,mitParent4);
     }
 
 }
